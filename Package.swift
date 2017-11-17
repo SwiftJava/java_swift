73079b49776d3d5bd1c75bdcc995fb48e775448c
Andriy Druk <andriy.druk@gmail.com>
2017-11-17 19:01:30 +0200

    Dev: change CJavaVM version to 1.1.3

--- Package.swift
+++ Package.swift
@@ -11,6 +11,6 @@ import PackageDescription
 let package = Package(
     name: "java_swift",
     dependencies: [
-        .Package(url: "https://github.com/SwiftJava/CJavaVM.git", versions: Version(1,0,0)..<Version(10,0,0)),
+        .Package(url: "https://github.com/SwiftJava/CJavaVM.git", version: Version(1,1,3)),
         ]
 )
